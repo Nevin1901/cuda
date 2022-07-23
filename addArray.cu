97fca5159e1516a2652f91c36fa35c70b2649804
Nevin1901 <nevinpuri1901@gmail.com>
2022-07-23 17:29:37 +0200

    1ms add

---
+++ addArray.cu
@@ -0,0 +1,45 @@
+#include <iostream>
+
+__global__ void populateVec(int n, float *a, float *b) {
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+
+    for (int i = index; i < n; i+= stride) {
+        a[i] = 2;
+        b[i] = 5;
+    }
+}
+
+__global__ void vecAdd(int n, float *a, float *b, float *c) {
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;
+
+    for (int i = index; i < n; i+= stride) {
+        c[i] = a[i] + b[i];
+    }
+}
+
+int main() {
+    int n = 1<<20;
+    int blockSize = 512;
+    int numBlocks = (n + blockSize - 1) / blockSize;
+
+    float *a, *b, *c;
+
+    cudaMallocManaged(&a, n * sizeof(float));
+    cudaMallocManaged(&b, n * sizeof(float));
+    cudaMallocManaged(&c, n * sizeof(float));
+
+    populateVec<<<numBlocks, blockSize>>>(n, a, b);
+    vecAdd<<<numBlocks, blockSize>>>(n, a, b, c);
+
+    cudaDeviceSynchronize();
+
+    for (int i = 0; i < 256; i++) {
+        std::cout << c[i] << std::endl;
+    }
+
+    return 0;
+
+    // vecAdd<<<1, 256>>>()
+}
