cdf045ceee2531e4974cd368337a29fe80a4fb9a
Nevin1901 <nevinpuri1901@gmail.com>
2022-07-25 23:10:46 +0200

    update line function

--- addArray.cu
+++ addArray.cu
@@ -2,6 +2,68 @@
 #include <X11/Xlib.h>
 #include <unistd.h>
 
+#define HEIGHT 10
+#define WIDTH 30
+
+__global__ void reset(char *screen) {
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    screen[y * WIDTH + x] = '.';
+}
+
+class Point2d {
+    public:
+        int x;
+        int y;
+
+        Point2d(int _x, int _y) {
+            x = _x;
+            y = _y;
+        }
+};
+
+class Screen2d {
+    public:
+        Screen2d(int _width, int _height) {
+            width = _width;
+            height = _height;
+
+            cudaMallocManaged(&screen, height * width * sizeof(int));
+            numBlocks = ((WIDTH * HEIGHT) + blockSize - 1) / blockSize;
+            drawBase();
+        }
+
+        void drawBase() {
+            reset<<<numBlocks, blockSize>>>(screen);
+
+            cudaDeviceSynchronize();
+
+            std::cout << "drew" << std::endl;
+        }
+
+        void line(int x1, int y1, int x2, int y2) {
+        }
+
+        void print() {
+            for (int i = 0; i < HEIGHT; i++) {
+                for (int k = 0; k < WIDTH; k++) {
+                    putchar(screen[i * WIDTH + k]);
+                    // screen[i * WIDTH + k] = '#';
+                }
+                putchar('\n');
+            }
+        }
+    
+    private:
+        int width;
+        int height;
+        char *screen;
+
+        int blockSize = 4;
+        int numBlocks;
+};
+
 __global__ void populateVec(int n, float *a, float *b) {
     int index = blockIdx.x * blockDim.x + threadIdx.x;
     int stride = blockDim.x * gridDim.x;
@@ -25,34 +87,33 @@ __global__ void vecAdd(int n, float *a, float *b, float *c) {
 }
 
 int main() {
-    Display *dpy = XOpenDisplay(0);
+    Screen2d screen(WIDTH, HEIGHT);
+    screen.drawBase();
+    screen.line(0, 0, 15, 5);
+    screen.print();
+    // char *screen = (char*)malloc(HEIGHT * WIDTH * sizeof(int));
 
-    int blackColor = BlackPixel(dpy, DefaultScreen(dpy));
-    int whiteColor = WhitePixel(dpy, DefaultScreen(dpy));
+    // for (int i = 0; i < HEIGHT; i++) {
+    //     for (int k = 0; k < WIDTH; k++) {
+    //         screen[i * WIDTH + k] = '.';
+    //     }
+    // }
 
-    Window w = XCreateSimpleWindow(dpy, DefaultRootWindow(dpy), 0, 0, 200, 100, 0, blackColor, blackColor);
+    // Point2d *point1 = new Point2d(0, 0);
+    // Point2d *point2 = new Point2d(WIDTH - 1, HEIGHT - 1);
 
-    XSelectInput(dpy, w, StructureNotifyMask);
 
-    XMapWindow(dpy, w);
+    // screen[0 * WIDTH + 0] = '#';
+    // screen[(HEIGHT - 1) * WIDTH + (WIDTH - 1)] = '#';
 
-    GC gc = XCreateGC(dpy, w, 0, 0);
+    // for (int i = 0; i < HEIGHT; i++) {
+    //     for (int k = 0; k < WIDTH; k++) {
+    //         putchar(screen[i * WIDTH + k]);
+    //         // screen[i * WIDTH + k] = '#';
+    //     }
+    //     putchar('\n');
+    // }
 
-    XSetForeground(dpy, gc, whiteColor);
-
-    for (;;) {
-        XEvent e;
-        XNextEvent(dpy, &e);
-        if (e.type == MapNotify) {
-            break;
-        }
-    }
-
-    XDrawLine(dpy, w, gc, 10, 60, 180, 20);
-
-    XFlush(dpy);
-
-    sleep(10);
     // int nDevices;
 
     // cudaGetDeviceCount(&nDevices);
@@ -63,26 +124,26 @@ int main() {
     //     std::cout << prop.name << std::endl;
     // }
 
-    int n = 1<<20;
-    int blockSize = 512;
-    int numBlocks = (n + blockSize - 1) / blockSize;
+    // int n = 1<<20;
+    // int blockSize = 512;
+    // int numBlocks = (n + blockSize - 1) / blockSize;
 
-    float *a, *b, *c;
+    // float *a, *b, *c;
 
-    cudaMallocManaged(&a, n * sizeof(float));
-    cudaMallocManaged(&b, n * sizeof(float));
-    cudaMallocManaged(&c, n * sizeof(float));
+    // cudaMallocManaged(&a, n * sizeof(float));
+    // cudaMallocManaged(&b, n * sizeof(float));
+    // cudaMallocManaged(&c, n * sizeof(float));
 
-    populateVec<<<numBlocks, blockSize>>>(n, a, b);
-    vecAdd<<<numBlocks, blockSize>>>(n, a, b, c);
+    // populateVec<<<numBlocks, blockSize>>>(n, a, b);
+    // vecAdd<<<numBlocks, blockSize>>>(n, a, b, c);
 
-    cudaDeviceSynchronize();
+    // cudaDeviceSynchronize();
 
-    // for (int i = 0; i < 256; i++) {
-    //     std::cout << c[i] << std::endl;
-    // }
+    // // for (int i = 0; i < 256; i++) {
+    // //     std::cout << c[i] << std::endl;
+    // // }
 
-    return 0;
+    // return 0;
 
     // vecAdd<<<1, 256>>>()
 }
